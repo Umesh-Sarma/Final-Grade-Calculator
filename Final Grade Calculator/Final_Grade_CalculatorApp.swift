9913c489422f22a5f8bfd450abb391aba0beb714
Umesh Sarma <usarma1200@gmail.com>
2023-10-23 10:03:59 -0500

    Initial Commit

---
+++ Final Grade Calculator/Final_Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Grade_CalculatorApp.swift
+//  Final Grade Calculator
+//
+//  Created by Umesh Sarma on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
